a637d03ab67e4a12da8c7b8ce71db695abd05a61
stanJ <cjiao@3ti.us>
2018-02-27 19:01:45 +0800

    singer

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -6,7 +6,7 @@
           <slider :loop="true" :auto-play="true">
             <div v-for="item in recommends">
               <a :href="item.linkUrl">
-                <img @load="loadImage" :src="item.picUrl">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl">
               </a>
             </div>
           </slider>
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="item in discList" class="item">
               <div class="icon">
-                <img width="60" height="60" :src="item.imgurl">
+                <img width="60" height="60" v-lazy="item.imgurl">
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
@@ -26,12 +26,16 @@
           </ul>
         </div>
       </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
     </scroll>
     <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Loading from 'base/loading/loading'
   import Scroll from 'base/scroll/scroll'
   import Slider from 'base/slider/slider'
   import {getRecommend, getDiscList} from 'api/recommend'
@@ -45,9 +49,7 @@
       }
     },
     created() {
-      setTimeout(() => {
-        this._getRecommend()
-      }, 2000)
+      this._getRecommend()
       this._getDiscList()
     },
     methods: {
@@ -74,7 +76,8 @@
     },
     components: {
       Slider,
-      Scroll
+      Scroll,
+      Loading
     }
   }
 </script>
